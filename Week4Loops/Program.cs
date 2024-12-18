815d4ab88d86c40acd7acefaf6be76cd426ba60d
Sten Eric Persidski <sten.eric.persidski@tktk.ee>
2024-12-18 17:28:23 +0200

    add week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//FOR-LOOP
+//DRY-principle - Do Not Repeat Yourself
+
+int i = 0;
+
+for(int i = 0; i < 5; i++) //i++ = i + 1
+{
+    Console.WriteLine("We dont need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
+
+//Console.WriteLine("We dont need no education.");
+//Console.WriteLine("We dont need no education.");
+//Console.WriteLine("We dont need no education.");
+//Console.WriteLine("We dont need no education.");
+//Console.WriteLine("We dont need no education.");
+
